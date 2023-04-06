a9ac04c2a8812e2cf777a0fc5cb8d5c53949d7d0
Garima <gpandey@unomaha.edu>
2023-04-06 06:45:02 -0400

    search filter implemented

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RandomUser from './components/RandomUsers';
+
+function App() {
+  return (
+    <div>
+      <RandomUser />
+    </div>
+  );
+}
+
+export default App;
